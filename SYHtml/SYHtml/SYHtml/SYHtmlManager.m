a758aa2ef3017cbecf741bb67a4448966df187f7
tsy <tsyios@163.com>
2016-11-15 18:38:52 +0800

    http

--- SYHtml/SYHtml/SYHtml/SYHtmlManager.m
+++ SYHtml/SYHtml/SYHtml/SYHtmlManager.m
@@ -14,9 +14,56 @@ SYSingleton_implementation(SYHtmlManager)
 - (void)requestData {
     NSURL *url = [NSURL URLWithString:@"https://www.aa924.com/htm/vodlist1"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10];
-   NSURLSessionTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-       NSLog(@"%@----%@",response,data);
-   }];
+//   NSURLSessionTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//       NSLog(@"%@----%@",response,data);
+//   }];
+//    __strong self = self;
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request];
     [task resume];
+}
+
+- (void)URLSession:(NSURLSession *)session
+didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler
+{
+    //AFNetworking中的处理方式
+    NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+    __block NSURLCredential *credential = nil;
+    //判断服务器返回的证书是否是服务器信任的
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+        credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+        /*disposition：如何处理证书
+         NSURLSessionAuthChallengePerformDefaultHandling:默认方式处理
+         NSURLSessionAuthChallengeUseCredential：使用指定的证书    NSURLSessionAuthChallengeCancelAuthenticationChallenge：取消请求
+         */
+        if (credential) {
+            disposition = NSURLSessionAuthChallengeUseCredential;
+        } else {
+            disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        }
+    } else {
+        disposition = NSURLSessionAuthChallengeCancelAuthenticationChallenge;
+    }
+    //安装证书
+    if (completionHandler) {
+        completionHandler(disposition, credential);
+    }
+}
+// 接收到服务器的响应
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler
+{
+    NSLog(@"didReceiveResponse");
+    completionHandler(NSURLSessionResponseAllow);
+}
+// 接收到服务器返回的数据
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
+{
+    NSLog(@"didReceiveData");
+}
+// 请求完毕
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+    NSLog(@"didCompleteWithError");
 }
 @end
