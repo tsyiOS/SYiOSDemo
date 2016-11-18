597937797985d6bf5ce0be6b58d078be46390a0f
tsy <tsyios@163.com>
2016-11-18 18:04:50 +0800

    test

---
+++ SYTestPop/SYTestPop/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  SYTestPop
+//
+//  Created by leju_esf on 16/11/18.
+//  Copyright © 2016年 tsy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIPopoverPresentationController
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
